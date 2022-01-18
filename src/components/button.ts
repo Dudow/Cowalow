1ff1976b11450ba7cc967f96ea7d2a6b4e769d6a
Dudow <edu30.nm@gmail.com>
2022-01-18 11:09:53 -0300

    feat: create button and input components

---
+++ src/components/button.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: transparent;
+  border: 2px solid #0099ff;
+  border-radius: 3px;
+  color: #fdfdfd;
+  font-size: 1.2rem;
+  margin: 1rem;
+  padding: 1rem 1.5rem;
+  cursor: pointer;
+`;
+
+export default Button;
---
+++ src/components/input.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  background: transparent;
+  border: 2px solid #0099ff;
+  border-radius: 3px;
+  color: #fdfdfd;
+  font-size: 1.2rem;
+  margin: 1rem;
+  padding: 1rem 1.5rem;
+  transition: 0.2s;
+`;
+
+export default Input;
