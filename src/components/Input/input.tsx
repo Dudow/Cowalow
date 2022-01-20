6116a959b04248bf3fe2f6656717231bed6dbce9
Dudow <edu30.nm@gmail.com>
2022-01-20 16:55:57 -0300

    feat: create mask input option

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,8 +1,19 @@
-import { useEffect, useRef } from 'react';
+import { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { useField } from '@unform/core';
+import InputMask from 'react-input-mask';
 import InputBase from './inputStyle';
 
-export default function Input({ name, placeholder, readOnly }: any) {
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  mask?: string;
+}
+
+const Input: React.FC<IInputProps> = ({
+  name,
+  placeholder,
+  mask = '',
+  ...props
+}: IInputProps) => {
   const inputRef = useRef(null);
 
   const { fieldName, defaultValue = '', registerField } = useField(name);
@@ -14,13 +25,23 @@ export default function Input({ name, placeholder, readOnly }: any) {
       path: 'value',
     });
   }, [fieldName, registerField]);
+
   return (
-    <InputBase
-      ref={inputRef}
-      id={fieldName}
-      defaultValue={defaultValue}
-      placeholder={placeholder}
-      readOnly={readOnly}
-    />
+    <InputMask
+      mask={mask}
+      maskPlaceholder={null}
+      value={props.value}
+      {...props}
+    >
+      <InputBase
+        ref={inputRef}
+        id={fieldName}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        onChange={props.onChange}
+      />
+    </InputMask>
   );
-}
+};
+
+export default Input;
